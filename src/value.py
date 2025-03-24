2a441eff04dc12dbabe8706faa22d7d59c60686e
Neo Koda <mneocicerok@gmail.com>
2025-03-25 05:34:40 +0700

    feat : more activation functions, initial weighting

--- src/value.py
+++ src/value.py
@@ -1,3 +1,5 @@
+import math
+
 class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
@@ -78,10 +80,53 @@ class Value:
             v._backward()
 
     def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        out = Value(0 if self.data < 0 else self.data, (self,), 'relu')
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
         out._backward = _backward
 
         return out
+    
+    def sigmoid(self):
+        s = 1 / (1 + math.exp(-self.data))
+        out = Value(s, (self,), 'sigmoid')
+
+        def _backward():
+            self.grad += s * (1 - s) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        t = math.tanh(self.data)
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t ** 2) * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def leaky_relu(self):
+        out = Value(0.01 * self.data if self.data < 0 else self.data, (self,), 'leaky_relu')
+
+        def _backward():
+            self.grad += (1 if out.data > 0 else 0.01) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def swish(self):
+        s = 1 / (1 + math.exp(-self.data))
+        sw = s * self.data
+        out = Value(sw, (self,), 'swish')
+
+        def _backward():
+            self.grad += ((s * (1 - s)) * self.data + s) * out.grad
+        out._backward = _backward
+
+        return out
+
+
+        

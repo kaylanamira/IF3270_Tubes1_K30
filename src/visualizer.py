a3ec5bda608bdc5ac26298e5aecded317622e7a5
kaylanamira <kaylanmr04@gmail.com>
2025-03-28 02:00:26 +0700

    refactor: display gradient as edge labels in graph

--- src/visualizer.py
+++ src/visualizer.py
@@ -31,11 +31,11 @@ class Visualizer:
 
                 for prev_neuron_idx in range(neuron.n_in):
                     prev_neuron_id = f"L{layer_idx}_N{prev_neuron_idx+1}"
-                    self.graph.add_edge(prev_neuron_id, neuron_id, weight=round(neuron.w[prev_neuron_idx].data, 2))
+                    self.graph.add_edge(prev_neuron_id, neuron_id, weight=round(neuron.w[prev_neuron_idx].data, 2), grad=round(neuron.w[prev_neuron_idx].grad, 2))
 
                 if (layer_idx>0):
                     neuron_bias_id = f"B{layer_idx}"
-                    self.graph.add_edge(neuron_bias_id, neuron_id, weight=round(neuron.b.data, 2))
+                    self.graph.add_edge(neuron_bias_id, neuron_id, weight=round(neuron.b.data, 2), grad=round(neuron.b.grad, 2))
 
     def plot_neural_network(self):
         self.get_network_graph()
@@ -44,7 +44,7 @@ class Visualizer:
         plt.figure(figsize=(12, 8))
         nx.draw(self.graph, self.pos, with_labels=True, node_color=node_color_list, node_size=1000, edge_color="gray")
 
-        edge_labels = {(src, dest): data["weight"] for src, dest, data in self.graph.edges(data=True)}
+        edge_labels = {(src, dest): f"w={data['weight']}, g={data['grad']}" for src, dest, data in self.graph.edges(data=True)}
         nx.draw_networkx_edge_labels(self.graph, self.pos, edge_labels=edge_labels, font_color="red", font_size=10, label_pos=0.2)
         
         plt.show()

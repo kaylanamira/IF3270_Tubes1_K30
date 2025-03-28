3c61798ba9d286075e3519cc6cd98cda1dafa07e
angiekierra <angie.kierra@gmail.com>
2025-03-28 20:34:06 +0700

    feat : change no auto grad

---
+++ src/miaw.py
@@ -0,0 +1,225 @@
+# NumPy-based FFNN with enhancements
+import numpy as np
+import json
+from tqdm import tqdm
+
+# Activation functions and derivatives
+def relu(x):
+    return np.maximum(0, x)
+
+def d_relu(x):
+    return (x > 0).astype(float)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def d_sigmoid(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+def tanh(x):
+    return np.tanh(x)
+
+def d_tanh(x):
+    return 1 - np.tanh(x) ** 2
+
+def linear(x):
+    return x
+
+def d_linear(x):
+    return np.ones_like(x)
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return e_x / np.sum(e_x, axis=1, keepdims=True)
+
+def d_softmax_crossentropy(y_pred, y_true):
+    return (y_pred - y_true) / y_true.shape[0]
+
+def get_activation(name):
+    return {
+        'relu': (relu, d_relu),
+        'sigmoid': (sigmoid, d_sigmoid),
+        'tanh': (tanh, d_tanh),
+        'linear': (linear, d_linear),
+        'softmax': (softmax, None)
+    }[name]
+
+# Loss functions
+def mse(y_pred, y_true):
+    return np.mean((y_pred - y_true) ** 2)
+
+def d_mse(y_pred, y_true):
+    return 2 * (y_pred - y_true) / y_true.shape[0]
+
+def bce(y_pred, y_true):
+    epsilon = 1e-12
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+def d_bce(y_pred, y_true):
+    epsilon = 1e-12
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return (y_pred - y_true) / (y_pred * (1 - y_pred) * y_true.shape[0])
+
+def cce(y_pred, y_true):
+    epsilon = 1e-12
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return -np.sum(y_true * np.log(y_pred)) / y_true.shape[0]
+
+class Layer:
+    def __init__(self, n_in, n_out, activation='linear', weight_init='xavier', use_rmsnorm=False):
+        self.activation_name = activation
+        self.act_fn, self.d_act_fn = get_activation(activation)
+        self.use_rmsnorm = use_rmsnorm
+
+        if weight_init == 'xavier':
+            limit = np.sqrt(6 / (n_in + n_out))
+            self.W = np.random.uniform(-limit, limit, (n_out, n_in))
+        elif weight_init == 'he':
+            std = np.sqrt(2 / n_in)
+            self.W = np.random.randn(n_out, n_in) * std
+        elif weight_init == 'uniform':
+            self.W = np.random.uniform(-1, 1, (n_out, n_in))
+        elif weight_init == 'normal':
+            self.W = np.random.randn(n_out, n_in)
+        elif weight_init == 'zero':
+            self.W = np.zeros((n_out, n_in))
+        else:
+            raise ValueError(f"Unknown weight_init: {weight_init}")
+
+        self.b = np.zeros((n_out,))
+        self.cache = {}
+
+    def rmsnorm(self, x, epsilon=1e-5):
+        mean_square = np.mean(x ** 2, axis=1, keepdims=True)
+        norm = x / np.sqrt(mean_square + epsilon)
+        return norm
+
+    def forward(self, x):
+        z = x @ self.W.T + self.b
+        if self.use_rmsnorm:
+            z = self.rmsnorm(z)
+        a = self.act_fn(z) if self.activation_name != 'softmax' else softmax(z)
+        self.cache = {'x': x, 'z': z, 'a': a}
+        return a
+
+    def backward(self, grad_output, y_true=None):
+        x = self.cache['x']
+        z = self.cache['z']
+        a = self.cache['a']
+
+        if self.activation_name == 'softmax':
+            dz = d_softmax_crossentropy(a, y_true)
+        else:
+            dz = grad_output * self.d_act_fn(z)
+
+        dW = dz.T @ x / x.shape[0]
+        db = np.mean(dz, axis=0)
+        dx = dz @ self.W
+        return dx, dW, db
+
+class FFNN:
+    def __init__(self, layer_sizes, activations, loss_function='mse', weight_init='xavier', use_rmsnorm=False):
+        self.layers = []
+        for i in range(len(layer_sizes) - 1):
+            self.layers.append(Layer(layer_sizes[i], layer_sizes[i+1], activations[i], weight_init, use_rmsnorm))
+
+        self.loss_function = loss_function
+        if loss_function == 'mse':
+            self.loss_fn = mse
+            self.d_loss_fn = d_mse
+        elif loss_function == 'bce':
+            self.loss_fn = bce
+            self.d_loss_fn = d_bce
+        elif loss_function == 'cce':
+            self.loss_fn = cce
+            self.d_loss_fn = None
+        else:
+            raise ValueError(f"Unsupported loss function: {loss_function}")
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self, y_pred, y_true):
+        grads = []
+        grad = self.d_loss_fn(y_pred, y_true) if self.d_loss_fn else None
+
+        for i in reversed(range(len(self.layers))):
+            if self.layers[i].activation_name == 'softmax':
+                grad, dW, db = self.layers[i].backward(grad_output=None, y_true=y_true)
+            else:
+                grad, dW, db = self.layers[i].backward(grad)
+            grads.append((dW, db))
+        return grads[::-1]
+
+    def update_weights(self, grads, lr, reg_type=None, lambda_reg=0.0):
+        for i, (layer, (dW, db)) in enumerate(zip(self.layers, grads)):
+            if reg_type == 'l2':
+                dW += lambda_reg * layer.W
+            elif reg_type == 'l1':
+                dW += lambda_reg * np.sign(layer.W)
+            layer.W -= lr * dW
+            layer.b -= lr * db
+
+    def fit(self, x, y, epochs=100, lr=0.01, batch_size=32, verbose=1, reg_type=None, lambda_reg=0.0):
+        history = {'loss': [], 'batch_losses': []}
+        for epoch in range(epochs):
+            indices = np.arange(x.shape[0])
+            np.random.shuffle(indices)
+            x, y = x[indices], y[indices]
+            batch_losses = []
+
+            if verbose:
+                pbar = tqdm(total=x.shape[0], desc=f"Epoch {epoch+1}/{epochs}", ncols=100)
+
+            for i in range(0, x.shape[0], batch_size):
+                xb = x[i:i+batch_size]
+                yb = y[i:i+batch_size]
+
+                y_pred = self.forward(xb)
+                loss = self.loss_fn(y_pred, yb)
+
+                if reg_type == 'l2':
+                    loss += 0.5 * lambda_reg * sum(np.sum(layer.W ** 2) for layer in self.layers)
+                elif reg_type == 'l1':
+                    loss += lambda_reg * sum(np.sum(np.abs(layer.W)) for layer in self.layers)
+
+                grads = self.backward(y_pred, yb)
+                self.update_weights(grads, lr, reg_type, lambda_reg)
+
+                batch_losses.append(loss)
+
+                if verbose:
+                    pbar.update(xb.shape[0])
+
+            if verbose:
+                pbar.close()
+
+            avg_loss = np.mean(batch_losses)
+            history['loss'].append(avg_loss)
+            history['batch_losses'].append(batch_losses)
+            if verbose:
+                print(f"Epoch {epoch+1} Completed - Loss: {avg_loss:.4f}")
+
+        return history
+
+    def predict(self, x):
+        y_pred = self.forward(x)
+        if self.loss_function == "cce":
+            return np.argmax(y_pred, axis=1)
+        elif self.loss_function == "bce":
+            return (y_pred >= 0.5).astype(int).flatten()
+        elif self.loss_function == "mse":
+            return y_pred
+
+    def get_weights(self):
+        return [(layer.W.copy(), layer.b.copy()) for layer in self.layers]
+
+    def set_weights(self, weights):
+        for layer, (W, b) in zip(self.layers, weights):
+            layer.W = W
+            layer.b = b
+
